2438fd13fc6e85bd7967f57fc7ef32687cfe2389
Kargil Singh <Kargilsingh5@gmail.com>
2023-05-28 18:39:57 +0530

    v

--- db/connectdb.js
+++ db/connectdb.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 
 const url = "mongodb://127.0.0.1:27017/AdmissionPortalSystem"
-const live_url ="mongodb+srv://kargilsingh5:Kargil@123@cluster0.epzsgm7.mongodb.net/Admission_portal?retryWrites=true&w=majority"
+const live_URL ="mongodb+srv://kargilsingh5:kargil123@cluster0.epzsgm7.mongodb.net/Admission_portal?retryWrites=true&w=majority"
 
 
 const connectDB =()=>{
-    return mongoose.connect(live_url)
+    return mongoose.connect(live_URL)
     
     
     .then(()=>{
